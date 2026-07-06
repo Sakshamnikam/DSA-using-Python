9c261454eddf9f7b080945b7c9a491555e8efe81
Saksham Nikam <sakshamnikam2007@gmail.com>
2026-07-06 17:48:42 +0530

    Minimal changes in code

--- Basic/holow_rect.py
+++ Basic/holow_rect.py
@@ -4,7 +4,7 @@ m=6 #columns
 for i in range(0,n):
     for j in range(0,m):
         if(i==0 or i==n-1 or j==0 or j==m-1):
-            print('',end='')
+            print('*',end='')
         else:
             print(' ',end='')
     print()
