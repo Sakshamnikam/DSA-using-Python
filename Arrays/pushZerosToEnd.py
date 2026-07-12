e48f7a2f5b0c45e734c794e321eeea797ca8d0ad
Saksham Nikam <sakshamnikam2007@gmail.com>
2026-07-12 23:47:01 +0530

    Wave and pushzeroto end

---
+++ Arrays/pushZerosToEnd.py
@@ -0,0 +1,14 @@
+def pushZerosToEnd(arr):
+    count =0
+
+    for i in range(len(arr)):
+        if arr[i] != 0 :
+            arr[i],arr[count] = arr[count],arr[i]
+            count+=1
+        
+    return arr
+
+
+
+arr=[1,2,0,0,0,3,5,4]
+print(pushZerosToEnd(arr))
